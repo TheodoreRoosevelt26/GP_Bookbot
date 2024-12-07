f29b863dfc25d7612d19c1bc2a823ca82147a360
TheodoreRoosevelt26 <TR.theodore.roosevelt@proton.me>
2024-12-07 13:14:48 +0200

    Assignment 9: improvement on code

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 path_to_file = "books/frankenstein.txt"
 
 def word_count(file_contents):
-    words = file_contents.split()
-    return len(words)
+    return len(file_contents.split())
 
 def main():
     with open(path_to_file) as f:
