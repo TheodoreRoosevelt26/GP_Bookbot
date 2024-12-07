77137272a5bff0061d7a6db31e2bd4f893ddb12b
TheodoreRoosevelt26 <TR.theodore.roosevelt@proton.me>
2024-12-07 13:39:16 +0200

    Assignment 10

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ path_to_file = "books/frankenstein.txt"
 def word_count(file_contents):
     return len(file_contents.split())
 
+def character_count(file_contents):
+    counting_dic = {}
+    lowercase_list = list(file_contents.lower())
+    for character in lowercase_list:
+        if character in counting_dic:
+            counting_dic[character] += 1
+        else:
+            counting_dic[character] = 1
+    return counting_dic
+
+
 def main():
     with open(path_to_file) as f:
         file_contents = f.read()
     number_words = word_count(file_contents)
-    print(number_words)
+    print(f"Number of words in book: {number_words}")
+    number_char = character_count(file_contents)
+    print(f"Number of characters in book: {number_char}")
 
 main()
